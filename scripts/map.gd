3c61e5402dca066cbd6840a52da5d73f6aa4c626
serand2048 <serand228@gmail.com>
2026-06-08 20:18:50 +0300

    .

--- scripts/map.gd
+++ scripts/map.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_area_2d_body_entered(_body):
 	get_tree().change_scene_to_file("res://scenes/home.tscn")
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://scenes/bar.tscn")
